aa73e06dbbaf3893335e9f844d3535a2422056e7
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2023-07-12 21:40:19 -0600

    logo-favicon

--- seoConfig/index.js
+++ seoConfig/index.js
@@ -4,9 +4,9 @@ export default {
     "Inkly is a tattoo app",
     
   og: {
-    title: "Logspot - Opensource changelog template",
+    title: "Inkly Changelog",
     description:
-      "Logspot is an open source change log template made with Nuxt, Vue, and Tailwindcss.",
+      "Inkly Changelog.",
     image: "https://logspot.vercel.app/logspot-banner.png",
     url: "https://logspot.vercel.app",
   },
